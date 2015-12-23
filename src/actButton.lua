89d4393f2c87879bbd50bf04f7b81f06fc2bd9bb
Manuel Pascual <pascual.manuel@gmail.com>
2015-12-23 23:22:35 +0100

    rajustes version y log

--- src/actButton.lua
+++ src/actButton.lua
@@ -23,23 +23,35 @@ getIcon()
 ------------------------------------------------------------------------------]]
 function getIcon(panel)
   -- obtener datos reales del panel
+  local tempAct, hora, icono
   if panel.properties.handTemperature ~= 0 then
+    -- obtener datos reales del panel
+    tempAct =  panel.properties.handTemperature
+    hora = math.ceil((panel.properties.handTimestamp - os.time()) / 3600)
+    if hora < 0 then hora = 0 end
+    hora = string.format('%02d', hora)
     -- icono de temperatura manual
-    return iconoManual
+    icono = iconoManual
   elseif panel.properties.vacationTemperature ~= 0 then
     -- icono de temperatura vacaciones
-    return iconoVacaci
+    icono = iconoVacaci
+    tempAct =  panel.properties.vacationTemperature
+    hora = '__'
   else
     -- icono de temperatura programada
-    return iconoProgra
+    icono = iconoProgra
+    tempAct =  0
+    hora = '__'
   end
+  tempAct = string.format('%02d', tostring(tempAct))
+  return icono, tempAct..'ºC / '..hora..'h'
 end
 
 -- obtener zona actual
 local zona = fibaro:get(_selfId, 'ui.zonaLabel.value')
 local textoZona = string.sub(zona, string.find(zona, '-', 1) + 1, #zona)
 zona = string.sub(zona, 1, string.find(zona, '-', 1) - 1)
---ibaro:debug(string.find(zona, '-', 1))
+
 -- obtener modo actual
 local modo  = fibaro:get(_selfId, 'ui.modoLabel.value')
 
@@ -56,8 +68,8 @@ if modo == 'Manual' then
   tempAct =  panel.properties.handTemperature
   -- hora = os.date('%H', panel.properties.handTimestamp)
   hora = math.ceil((panel.properties.handTimestamp - os.time()) / 3600)
+  if hora < 0 then hora = 0 end
   hora = string.format('%02d', hora)
-  fibaro:debug('hora: '..hora)
 else -- si el modo es vacaciones actualizar vacationTemperature pero no mostrar
 -- handTimestamp
   tempAct =  panel.properties.vacationTemperature
@@ -70,8 +82,11 @@ fibaro:debug(tempAct..'ºC / '..hora..'h')
 fibaro:call(_selfId, "setProperty", 'ui.tempParaLabel.value',
   tempAct..'ºC / '..hora..'h')
 
--- refrescar icono recomendacion
-fibaro:call(_selfId, 'setProperty', "currentIcon", getIcon(panel))
+local iconoId, tempHora =  getIcon(panel)
+-- refrescar icono
+fibaro:call(_selfId, 'setProperty', "currentIcon", iconoId)
 
 --refrescar log
-fibaro:log(textoZona..' - '..tempAct..'ºC / '..hora..'h')
+fibaro:debug(textoZona..' - '..tempHora)
+--fibaro:log(textoZona..' - '..tempAct..'ºC / '..hora..'h')
+fibaro:log(textoZona..' - '..tempHora)
--- src/mainLoop.lua
+++ src/mainLoop.lua
@@ -10,17 +10,48 @@
 local _selfId = fibaro:getSelfId()  -- ID de este dispositivo virtual
 --[[----- FIN CONFIGURACION AVANZADA -----------------------------------------]]
 
+--[[
+getLog()
+  parametros: tabla con el panel
+	retorno: devuelve el id del icono correspondiente al tipo de programación
+------------------------------------------------------------------------------]]
+function getLog()
+  -- obtener zona
+  local zona, zonaId
+  zona = fibaro:get(_selfId, 'ui.zonaLabel.value')
+  zonaId = string.sub(zona, 1, string.find(zona, '-', 1) -1)
+  zona = string.sub(zona, string.find(zona, '-', 1) + 1, #zona)
+  -- obtener datos reales del panel
+  if not HC2 then
+    HC2 = Net.FHttp("127.0.0.1", 11111)
+  end
+  fibaro:debug(zonaId..'-'..zona)
+  response ,status, errorCode = HC2:GET("/api/panels/heating/"..zonaId)
+  local panel = json.decode(response)
+
+  local tempAct, hora, icono
+  if panel.properties.handTemperature ~= 0 then
+    -- obtener datos reales del panel
+    tempAct =  panel.properties.handTemperature
+    hora = math.ceil((panel.properties.handTimestamp - os.time()) / 3600)
+    if hora < 0 then hora = 0 end
+    hora = string.format('%02d', hora)
+  elseif panel.properties.vacationTemperature ~= 0 then
+    tempAct =  panel.properties.vacationTemperature
+    hora = '__'
+  else
+    tempAct =  0
+    hora = '__'
+  end
+  tempAct = string.format('%02d', tostring(tempAct))
+  return zona..' - '..tempAct..'ºC / '..hora..'h'
+end
 
 --[[--------BUCLE DE CONTROL -------------------------------------------------]]
 while true do
   --
   fibaro:sleep(1000)
-  -- obtener estado
-  local estado = fibaro:get(_selfId, 'ui.tempParaLabel.value')
-  -- obtener zona
-  local zona = fibaro:get(_selfId, 'ui.zonaLabel.value')
-  zona = string.sub(zona, string.find(zona, '-', 1) + 1, #zona)
   --refrescar log
-  fibaro:log(zona..' - '..estado)
+  fibaro:log(getLog())
 end
 --[[--------------------------------------------------------------------------]]
